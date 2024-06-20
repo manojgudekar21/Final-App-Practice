b41175e2754cbe6a20c2b7fe786634679440a2c3
manoj gudekar <manojgudekar21@gmail.com>
2024-06-20 13:48:31 +0530

    just to check the git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieDetailsComponent } from './recepies/recepie-details/recepie-details.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieDetailsComponent,
+    RecepieItemComponent,
   ],
   imports: [
     BrowserModule,
